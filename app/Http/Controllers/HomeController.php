c2126680dae1b5717a95c180b6ee5bab8d7ba475
Ryan Liew <fatal-fates@hotmail.com>
2018-05-11 00:09:12 +0800

    Fix undefined variable

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -17,7 +17,7 @@ class HomeController extends Controller
      */
     public function index()
     {
-        if($user->identification !== "")
+        if(auth()->user()->identification !== "")
             return redirect()->route('register.complete');
 
         return redirect()->route('register.success');
