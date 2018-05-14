faf991926f2ab33697ac5077611ddaec66665585
Ryan Liew <fatal-fates@hotmail.com>
2018-05-14 21:55:22 +0800

    Allow registered user to login with social media

--- app/Repositories/UserRepository.php
+++ app/Repositories/UserRepository.php
@@ -8,33 +8,37 @@ class UserRepository {
 
 	public function findByUsernameOrCreate($userData, $service)
 	{
+		$user = new User();
 		if(!empty($userData->email))
 		{
-			$user = User::updateOrCreate(['social_service' => $service, 'social_id' => $userData->id], 
-										 ['password' => bcrypt('123456'),
+			$user = User::updateOrCreate(['email' => $userData->email], 
+										 ['password' => bcrypt('newleaf'),
 									  	  'avatar_path' => $userData->avatar_original,
 										  'name' => $userData->name, 
-										  'email' => $userData->email ? $userData->email : $userData->id . '@email.com', 
+										  'email' => $userData->email, 
+										  'social_service' => $service,
+										  'social_id' => $userData->id
+										 ]
+										);	
+
+
+		}
+		else
+		{
+			
+			$user = User::updateOrCreate(['social_service' => $service, 'social_id' => $userData->id], 
+										 ['password' => bcrypt('newleaf'),
+									  	  'avatar_path' => $userData->avatar_original,
+										  'name' => $userData->name, 
+										  'email' => $userData->id . '@email.com', 
 										  'social_service' => $service,
 										  'social_id' => $userData->id
 										 ]
 										);
-			/*$user = User::where('email', $userData->email)->first();
-				
-			if(!$user)
-			{
-				$user = User::create([
-					'email' => $userData->email,
-					'password' => bcrypt('123456'),
-					'avatar_path' => $userData->avatar_original,
-					'name' => $userData->name
-				]);
-			}	*/	
 
-			return $user;
 		}
 
-		return false;
+		return $user;
 	}
 
 }
