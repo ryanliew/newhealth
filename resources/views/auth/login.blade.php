35a7b4bc4df47548e44dd65e32568690ba5a91cd
Ryan Liew <fatal-fates@hotmail.com>
2018-05-15 22:58:26 +0800

    Fixed small details error

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -17,14 +17,14 @@
     <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
     <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
     <![endif]-->
-<link rel="stylesheet" href="/css/separate/pages/login.min.css">
+<link rel="stylesheet" href="/css/separate/pages/login.min.css?v=1.0">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
     <link rel="stylesheet" href="/css/lib/bootstrap/bootstrap.min.css">
     <link rel="stylesheet" href="/css/start-ui.css">
     <link rel="stylesheet" href="/css/separate/buttons.css">
 </head>
 <body>
-    <div class="top-bar" style="position:fixed; right:0">
+    <div class="top-bar" style="position:fixed; right:0; z-index: 5;">
                             
         <div class="dropdown">
             <button class="btn btn-light dropdown-toggle" type="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
