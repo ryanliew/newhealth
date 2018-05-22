c9c3e2ef545ea7dcdfe3276d041f140e4c15586f
Ryan Liew <fatal-fates@hotmail.com>
2018-05-22 17:44:54 +0800

    Added versions to JS

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -25,8 +25,8 @@
     <body class="with-side-menu-compact">
         <div id="app"></div>
        
-        <script src="./js/form.js"></script> 
-        <script src="./js/app.js"></script>
+        <script src="./js/form.js?v=1.2"></script> 
+        <script src="./js/app.js?v=1.10"></script>
         <script src="./js/lib/tether/tether.min.js"></script> 
         <script src="./js/plugins.js"></script>
         <script src="./js/start-ui.js"></script>
