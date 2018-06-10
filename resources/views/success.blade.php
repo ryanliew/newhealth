2149959e33f84b22e601c6cf9d1b645bb8ec27fb
Ryan Liew <fatal-fates@hotmail.com>
2018-06-09 21:21:29 -0700

    Update terms and conditions language lines

--- resources/views/success.blade.php
+++ resources/views/success.blade.php
@@ -323,22 +323,22 @@
                                     <div class="col-sm">
                                         <div class="p-4">
                                             <ol>
-                                                <li>The purchaser undersigned (“Purchaser”) agrees to sign a sales contract in the form and manner as prescribed by Newleaf Plantation Berhad (Company No. 1251569-U) (“Company”) within twenty-one (21) working days from the date of this Purchase Form and to pay the Total Sales Price in full on or before signing of the sales contract, failing which the Company shall be entitled to terminate this Purchase Form and to forfeit the Total Sales Price absolutely.</li>
-                                                <li>The Company shall have a period of thirty (30) months from the date of the sales contract (“Conditional Period”) to source, identify and finalize the land in order to commence the project for durian plantation, failing which the Company shall fully refund the Total Sales Price received to the Purchaser, free of interest.</li>
-                                                <li>The Purchaser shall have the option to sell the purchased trees (“Trees”) back to the Company upon maturity of ten (10) years from the expiry of the Conditional Period at the sum equivalent to the Total Sales Price multiply by two hundred per centum (200%) (“Option Price”) and the Company shall purchase the Trees at the Option Price.</li>
-                                                <li>The Purchaser hereby agrees to appoint the Company and/or its nominee to be the operator to manage the Trees at its sole and absolute discretion and the Purchaser further agrees that the Company shall be entitled to thirty per centum (30%) of the profit deriving from the sale proceeds of the fruits of Trees after deducting all the necessary costs and expenses.</li>
-                                                <li>The Purchaser will be given a Quick Response (QR) code for the Trees in the sales contract and the necessary information for the Purchaser to access into the system for the latest update and information of the Trees. </li>
-                                                <li>After expiry of five (5) years from the date of sales contract and subject to the Company’s written consent, the sales contract shall be transferable to the Purchaser’s nominee upon the Purchaser’s written notice to the Company provided that the said nominee shall appoint the Company’s panel solicitors, Messrs Lee &amp; Partners of 19-2, Jalan PJU 8/5D, Perdana Business Centre, Bandar Damansara Perdana 47820 Petaling Jaya, Selangor, Malaysia and shall bear all the necessary legal costs and expenses.</li>
-                                                <li>The Purchaser shall fill up the nomination details in this Purchase Form and the Company shall transfer the sales contract to the nominee upon its receipt of the sales contract between the Purchaser and the Company and the death certificate of the Purchaser.</li>
-                                                <li>The Company shall not be liable to the Purchaser and/or his heirs, successors-in-title, assignees, personal representatives, in any manner whatsoever in relation to the nomination.</li>
-                                                <li>The Purchaser hereby warrants in favor of the Company that the Purchaser is not prevented or restrained legally, commercially or otherwise from purchasing the Trees from the Company.</li>
-                                                <li>The Purchaser hereby warrants that all information furnished in this Purchase Form is true, accurate and not misleading and the Purchaser shall promptly notify the Company if it ceases to be so and/or if there is any change to the information.</li>
-                                                <li>The Purchaser hereby indemnifies and holds the Company harmless against all losses, damages, costs, charges and expenses incurred or suffered by the Company, including costs on solicitors and client basis, in connection with and arising from the terms of this Purchase Form.</li>
-                                                <li>The Purchaser hereby gives consent to the Company in processing including but not limited to access, obtain, verify and/or utilise the personal data or information of the Purchaser in accordance with the Personal Data Protection Principles under the Personal Data Protection Act 2010.</li>
-                                                <li>This Purchase Form embodies terms and conditions as agreed upon between the parties as to the subject matter of this Purchase Form and supersedes all previous agreements (if any) between the parties with respect to the subject matter hereof, whether written or oral and no amendment or variation hereof shall be effective unless made by the parties in writing and signed by both parties.</li>
-                                                <li>Time wherever mentioned herein shall be of the essence.</li>
-                                                <li>This Purchase Form shall be governed by and construed in accordance with Malaysian law and the parties hereby submit to the exclusive jurisdiction of the courts of Malaysia.</li>
-                                                <li>This Purchase Form has been translated into Mandarin. If there is any inconsistency or discrepancies between the English version and the Mandarin version, the English version shall prevail.</li>
+                                                <li>{{ __('terms.term_1') }}</li>
+                                                <li>{{ __('terms.term_2') }}</li>
+                                                <li>{{ __('terms.term_3') }}</li>
+                                                <li>{{ __('terms.term_4') }}</li>
+                                                <li>{{ __('terms.term_5') }}</li>
+                                                <li>{{ __('terms.term_6') }}</li>
+                                                <li>{{ __('terms.term_7') }}</li>
+                                                <li>{{ __('terms.term_8') }}</li>
+                                                <li>{{ __('terms.term_9') }}</li>
+                                                <li>{{ __('terms.term_10') }}</li>
+                                                <li>{{ __('terms.term_11') }}</li>
+                                                <li>{{ __('terms.term_12') }}</li>
+                                                <li>{{ __('terms.term_13') }}</li>
+                                                <li>{{ __('terms.term_14') }}</li>
+                                                <li>{{ __('terms.term_15') }}</li>
+                                                <li>{{ __('terms.term_16') }}</li>
                                             </ol>
                                         </div>
                                     </div>
