9e29f2a6cd32e3f3c7ec17729a7dd39f79cb379a
Ryan Liew <fatal-fates@hotmail.com>
2018-06-20 17:36:16 +0800

    Bump up JS version

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -26,7 +26,7 @@
         <div id="app"></div>
        
         <script src="./js/form.js?v=1.2"></script> 
-        <script src="./js/app.js?v=1.23"></script>
+        <script src="./js/app.js?v=1.24"></script>
         <script src="./js/lib/tether/tether.min.js"></script> 
         <script src="./js/plugins.js"></script>
         <script src="./js/start-ui.js"></script>
