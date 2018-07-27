645a17ad57b5453f81fd4fe0578c8c3cf7bde55d
Ryan Liew <fatal-fates@hotmail.com>
2018-07-27 21:28:56 +0800

    Created user level component

---
+++ resources/assets/js/components/UserLevel.vue
@@ -0,0 +1,35 @@
+<template>
+	<div class="card">
+		<div class="card-header">
+			<div class="row align-items-center">
+				<div class="col">
+					{{ 'user.settings' | trans }}
+				</div>
+			</div>
+		</div>
+		<div class="card-block">
+			
+		</div>
+	</div>
+</template>
+
+<script>
+	import formButtonMixin from '../mixins/form-button.js';
+
+	export default {
+		props: ['selectedUser'],
+
+		mixins: [formButtonMixin],
+
+		data() {
+			return {
+				form: new Form({
+					parent_id: '',
+					user_level: '',
+					is_admin: '',
+				}),
+				user: window.user
+			};
+		},
+	}
+</script>
