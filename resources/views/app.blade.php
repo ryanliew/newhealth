daa1c788037323bf894e2e0f2b00e497b1bcb8da
Lun <lunwhl@hotmail.com>
2019-02-20 23:46:41 +0800

    version update

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -27,7 +27,7 @@
         <div id="app"></div>
        
         <script src="./js/form.js?v=1.2"></script> 
-        <script src="./js/app.js?v=1.9"></script>
+        <script src="./js/app.js?v=2.0"></script>
         <script src="./js/lib/tether/tether.min.js"></script> 
         <script src="./js/plugins.js"></script>
         <script src="./js/start-ui.js"></script>
